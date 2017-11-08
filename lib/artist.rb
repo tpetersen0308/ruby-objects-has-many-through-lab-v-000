7b598793d1b74a671ae8d0fbd59beb542f95b1a2
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-08 20:10:26 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_reader :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    self.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+  end
+
+  def genres
+    self.songs.collect { |song| song.genre }
+  end
+
+end
+
+  
